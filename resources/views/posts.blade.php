3a53e361438fef0a2f08fd7cc6a2df4def7b0cc6
Avinda Alamsyah <avindaalamsyah@gmail.com>
2022-12-21 21:40:46 +0700

    feat(posts): add posts page

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posts</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <article>
+        <h1>First Post</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam adipisci beatae non sint placeat facere a accusantium inventore autem sit cumque, voluptatem modi officia error dolorem blanditiis, unde culpa debitis?</p>
+    </article>
+    <article>
+        <h1>Second Post</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam adipisci beatae non sint placeat facere a accusantium inventore autem sit cumque, voluptatem modi officia error dolorem blanditiis, unde culpa debitis?</p>
+    </article>
+    <article>
+        <h1>Third Post</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam adipisci beatae non sint placeat facere a accusantium inventore autem sit cumque, voluptatem modi officia error dolorem blanditiis, unde culpa debitis?</p>
+    </article>
+</body>
+
+</html>
